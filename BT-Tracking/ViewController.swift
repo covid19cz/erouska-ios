5cfefce41677197957feb2a7866b589bf225fe84
Lukas Foldyna <lfoldyna@gmail.com>
2020-03-14 18:17:40 +0100

    Initial Commit

---
+++ BT-Tracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BT-Tracking
+//
+//  Created by Lukáš Foldýna on 14/03/2020.
+//  Copyright © 2020 Covid19CZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
