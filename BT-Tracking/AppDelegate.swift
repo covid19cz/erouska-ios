dd6112eda8f23eef68a4d74afa556f4806eb4840
Lukas Foldyna <lfoldyna@gmail.com>
2020-03-17 13:47:35 +0100

    BG info

--- BT-Tracking/AppDelegate.swift
+++ BT-Tracking/AppDelegate.swift
@@ -11,6 +11,9 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    private var backgroundTask: UIBackgroundTaskIdentifier = .invalid
+    private var inBackgroundStage: Bool = false
+
     private var rootWindow: UIWindow? = nil
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -48,10 +51,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         log("\n\n\n-FOREGROUND---------------------------\n")
+
+        if backgroundTask != .invalid {
+            application.endBackgroundTask(backgroundTask)
+            backgroundTask = .invalid
+        }
+        inBackgroundStage = false
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
+        //log("\n\n\n-BACKGROUND---------------------------\n")
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        NSLog("we are in the background...")
         log("\n\n\n-BACKGROUND---------------------------\n")
+
+        inBackgroundStage = true
+        backgroundTask = application.beginBackgroundTask(withName: "BT") {
+            log("\n\n\n-EXPIRATION TASK---------------------------\n")
+        }
+
+        DispatchQueue.global(qos: .background).async {
+            while(true) {
+                if self.inBackgroundStage == false {
+                    log("\n\n\n-END TASK---------------------------\n")
+                    break
+                }
+            }
+
+            application.endBackgroundTask(self.backgroundTask)
+            self.backgroundTask = .invalid
+        }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
