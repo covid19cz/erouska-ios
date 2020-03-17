f400f9489525a8185710a8a1d6f5a48b5c026d48
Lukas Foldyna <lfoldyna@gmail.com>
2020-03-17 14:55:44 +0100

    Fixed background messages

--- BT-Tracking/AppDelegate.swift
+++ BT-Tracking/AppDelegate.swift
@@ -19,36 +19,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         log("\n\n\n-START--------------------------------\n")
 
-        if #available(iOS 13, *) {
-            // using scene
-        } else {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
 
-            let window = UIWindow()
-            window.backgroundColor = .white
-            window.makeKeyAndVisible()
-            window.rootViewController = storyboard.instantiateInitialViewController()
-            self.rootWindow = window
-        }
+        let window = UIWindow()
+        window.backgroundColor = .white
+        window.makeKeyAndVisible()
+        window.rootViewController = storyboard.instantiateInitialViewController()
+        self.rootWindow = window
+
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         log("\n\n\n-FOREGROUND---------------------------\n")
 
@@ -60,7 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        //log("\n\n\n-BACKGROUND---------------------------\n")
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
