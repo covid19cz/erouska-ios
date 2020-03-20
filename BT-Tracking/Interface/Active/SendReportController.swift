8fe4c05fab955edf563c0c8f20d72fe5b493495f
Lukas Foldyna <lfoldyna@gmail.com>
2020-03-20 11:44:29 +0100

    Link active storyboard

---
+++ BT-Tracking/Interface/Active/SendReportController.swift
@@ -0,0 +1,17 @@
+//
+//  SendReportController.swift
+//  BT-Tracking
+//
+//  Created by Lukáš Foldýna on 20/03/2020.
+//  Copyright © 2020 Covid19CZ. All rights reserved.
+//
+
+import UIKit
+
+class SendReportController: UIViewController {
+
+    @IBAction private func closeAction() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
