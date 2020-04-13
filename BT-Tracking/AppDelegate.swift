bc363d0a9a04d72d39c00b745bd7341d272467bb
Tomas Svoboda <teeesone@gmail.com>
2020-04-13 11:19:45 +0200

    PR feedback

--- BT-Tracking/AppDelegate.swift
+++ BT-Tracking/AppDelegate.swift
@@ -78,10 +78,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         log("\n\n\n-START--------------------------------\n")
 
+        clearKeychainIfNeeded()
         generalSetup()
         setupInterface()
         setupBackgroundMode(for: application)
-        clearKeychainIfNeeded()
         
         return true
     }
