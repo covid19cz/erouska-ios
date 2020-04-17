254701c15e513db8f58db2d6ca43a9e88fcbc8fb
Michal Srutek <michal.srutek@outlook.com>
2020-04-17 11:43:32 +0200

    Color RSSI exposure

--- BT-Tracking/Services/Store/Scan.swift
+++ BT-Tracking/Services/Store/Scan.swift
@@ -20,4 +20,31 @@ struct Scan: Equatable {
     let rssi: Int
     let medianRssi: Int?
     let state: BTScanDevice.State
+
+    enum ExpositionLevel: Int {
+        case level1
+        case level2
+        case level3
+        case level4
+        case level5
+        case level6
+        case level7
+        case level8
+    }
+
+    var expositionLevel: ExpositionLevel {
+        let criticalExpositionRssi = RemoteValues.criticalExpositionRssi
+
+        switch rssi {
+        case criticalExpositionRssi...: return .level8
+        case (criticalExpositionRssi - 5)...: return .level7
+        case (criticalExpositionRssi - 10)...: return .level6
+        case (criticalExpositionRssi - 15)...: return .level5
+        case (criticalExpositionRssi - 17)...: return .level4
+        case (criticalExpositionRssi - 19)...: return .level3
+        case (criticalExpositionRssi - 21)...: return .level2
+        default: return .level1
+        }
+    }
+
 }
