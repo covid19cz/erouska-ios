3865248a7e42f480a673d6a171277dc413b0b7a5
Lukas Foldyna <lfoldyna@gmail.com>
2020-04-18 10:01:13 +0200

    Localized signup part 1

---
+++ BT-Tracking/Extensions/UIKit/Localization.swift
@@ -0,0 +1,45 @@
+//
+//  Localization.swift
+//  eRouska Dev
+//
+//  Created by Lukáš Foldýna on 18/04/2020.
+//  Copyright © 2020 Covid19CZ. All rights reserved.
+//
+
+import UIKit
+
+func Localizable(_ text: String, comment: String? = nil) -> String {
+    return NSLocalizedString(text, comment: comment ?? text)
+}
+
+extension UINavigationItem {
+
+    func localizedTitle(_ text: String, comment: String? = nil) {
+        title = Localizable(text, comment: comment)
+    }
+
+}
+
+extension UIBarButtonItem {
+
+    func localizedTitle(_ text: String, comment: String? = nil) {
+        title = Localizable(text, comment: comment)
+    }
+
+}
+
+extension UIButton {
+
+    func localizedTitle(_ text: String, comment: String? = nil) {
+        setTitle(Localizable(text, comment: comment), for: .normal)
+    }
+
+}
+
+extension UILabel {
+
+    func localizedText(_ text: String, comment: String? = nil) {
+        self.text = Localizable(text, comment: comment)
+    }
+
+}
