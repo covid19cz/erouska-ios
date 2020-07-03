e992a8e7abbef2603256fc884e8e2b74188b9ed4
Lukas Foldyna <lfoldyna@gmail.com>
2020-07-03 17:10:09 +0200

    Reorganizaiton

---
+++ BT-Tracking/Services/CountryCodes/CountryCodes.swift
@@ -0,0 +1,10 @@
+//
+//  CountryCodes.swift
+//  eRouska Dev
+//
+//  Created by Lukáš Foldýna on 03/07/2020.
+//  Copyright © 2020 Covid19CZ. All rights reserved.
+//
+
+import Foundation
+import CoreTelephony
