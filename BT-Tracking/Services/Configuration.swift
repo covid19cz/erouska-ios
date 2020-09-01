139d6e70a4d9e47f3f762722fb3c71d98792ed98
Lukas Foldyna <lfoldyna@gmail.com>
2020-09-01 11:06:07 +0200

    Remove old key

--- BT-Tracking/Services/Configuration.swift
+++ BT-Tracking/Services/Configuration.swift
@@ -29,25 +29,3 @@ struct Configuration {
     let verificationDeviceKey: String = "Ar9VQ1tZS1ANU0LLPGw8nUnavJNBDCaTGEaEQbydvTYFgnW7oqQkTCLUxhk6azLm8IjTtCRVqQIi/wNscvniGw"
 
 }
-
-struct ExposureConfiguration {
-
-    let factorHigh = 0.17
-    let factorStandard = 1
-    let factorLow = 1.5
-    let lowerThreshold = 55
-    let higherThreshold = 63
-    let triggerThreshold = 15
-
-    var configuration: ENExposureConfiguration {
-        let configuration = ENExposureConfiguration()
-        configuration.minimumRiskScore = 0
-        configuration.attenuationLevelValues = [1, 2, 3, 4, 5, 6, 7, 8] as [NSNumber]
-        configuration.daysSinceLastExposureLevelValues = [1, 2, 3, 4, 5, 6, 7, 8] as [NSNumber]
-        configuration.durationLevelValues = [1, 2, 3, 4, 5, 6, 7, 8] as [NSNumber]
-        configuration.transmissionRiskLevelValues = [1, 2, 3, 4, 5, 6, 7, 8] as [NSNumber]
-        configuration.metadata = ["attenuationDurationThresholds": [lowerThreshold, higherThreshold]]
-        return configuration
-    }
-
-}
