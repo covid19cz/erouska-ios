046746713c3f981f99cbb37673c9b435cbdcf365
Lukas Foldyna <lfoldyna@gmail.com>
2021-07-24 16:17:19 +0200

    WIP widget

--- eRouska Widget/CurrentDataWidget.swift
+++ eRouska Widget/CurrentDataWidget.swift
@@ -8,41 +8,49 @@
 import WidgetKit
 import SwiftUI
 
-struct CurrentDataEntryView : View {
+struct CurrentDataEntryView: View {
     var entry: AppStateProvider.Entry
 
     var body: some View {
         VStack {
-            Image(entry.imageName)
-                .resizable()
-                .frame(width: 56, height: 56, alignment: .center)
+            CurrentDataRowView(iconName: "test1", title: "Title 1", subtitle: "Subtitle 1")
 
-            Text(entry.description)
-                .font(.caption2).fontWeight(.medium)
-                .foregroundColor(entry.textColor)
-                .padding(.top, 2)
-                .padding(.bottom, 3)
-                .padding(.horizontal, 10)
-                .multilineTextAlignment(.center)
+            CurrentDataRowView(iconName: "test2", title: "Title 2", subtitle: "Subtitle 2")
 
-            Button(entry.actineTitle) {
+            CurrentDataRowView(iconName: "test3", title: "Title 3", subtitle: "Subtitle 3")
 
-            }
-            .font(.subheadline)
-            .padding(.horizontal, 20)
-            .padding(.vertical, 5)
-            .frame(minWidth: 110)
-            .background(
-                RoundedRectangle(cornerRadius: 16)
-                    .foregroundColor(Color("DasboardButton"))
-            )
+            CurrentDataRowView(iconName: "test4", title: "Title 4", subtitle: "Subtitle 4")
         }.widgetURL(entry.actionURL)
+    }
+}
+
+struct CurrentDataRowView: View {
+    let iconName: String
+    let title: String
+    let subtitle: String
+
+    var body: some View {
+        HStack(spacing: 16) {
+            Image(iconName)
+                .resizable()
+                .frame(width: 25, height: 25, alignment: .center)
+                .accentColor(Color("DataColor"))
+            VStack(alignment: .leading, spacing: 2) {
+                Text(title)
+                    .font(.footnote).fontWeight(.medium)
+                    .foregroundColor(.primary)
+                Text(subtitle)
+                    .font(.caption2).fontWeight(.regular)
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+        }.padding(.horizontal, 16)
     }
 }
 
 struct CurrentDataWidgetPreviews: PreviewProvider {
     static var previews: some View {
         CurrentDataEntryView(entry: AppStateEntry(state: .paused, configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
     }
 }
